b5db7ddf9a32f7ffc0c6e50e37b693ee1104f689
julia-ing <ywchoi0625@gmail.com>
2023-09-06 01:15:57 +0900

    add resizing logic

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+import io
 from fastapi import FastAPI, File
 from ocr import clova
+from PIL import Image
 
 app = FastAPI()
 
 @app.post("/receipt")
 async def receipt_scan(file: bytes = File()):
-    res = clova(file)
+    img = Image.open(io.BytesIO(file))
+    resized = img.resize((1080, 1440))
+    
+    buf = io.BytesIO()
+    resized.save(buf, format='PNG')
+
+    res = clova(buf.getvalue())
     return res
